adfdfc651bafa961bb60dbb8360ed2d8f5a2bcb0
MeenakshiMuthuraman <144947917+MeenakshiMuthuraman@users.noreply.github.com>
2023-09-28 15:33:10 -0400

    POM.xml-updated with new version

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -25,8 +25,9 @@ public class DriverFactory {
 		System.out.println("browser value is:"+browser);
 		if(browser.equals("chrome"))
 		{
-			WebDriverManager.chromedriver().setup();
-			//System.setProperty("webdriver.chrome.driver","../../Downloads/chromedriver-mac-x64/chromedriver");
+			//WebDriverManager.chromedriver().setup();
+			System.setProperty("webdriver.chrome.driver","../../Downloads/chromedriver-mac-x64-2/chromedriver");
+			//Users/venmeen/Downloads/chromedriver-mac-x64 2
 			//System.out.println("Updating Chrome Version");
 
 			tlDriver.set(new ChromeDriver());
